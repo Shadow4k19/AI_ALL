5300de35328529d532367191d6ee55faf4fbe121
Shadow4k19 <kongza15178@gmail.com>
2024-04-12 16:51:20 +0700

    Update-12-4-2024

--- BackEnd/AI_BackEnd_django/myproject/settings.py
+++ BackEnd/AI_BackEnd_django/myproject/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'PORT': config('DB_PORT', cast=int),  
     }
 }
 
